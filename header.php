3eaad48cf38b4bd4f869eab81698cedcea266465
Ben Kaminski <ben@benkaminski.com>
2018-07-06 18:24:49 -0400

    created front-page, header, and footer

---
+++ header.php
@@ -0,0 +1,27 @@
+<!doctype html>
+<html <?php language_attributes(); ?>>
+	<head>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+		<?php wp_head(); ?>
+	</head>
+	<body <?php body_class(); ?>>
+		<nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
+			<a class="navbar-brand" href="#"><?php echo get_bloginfo('name'); ?></a>
+			<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span>
+			</button>
+			<div class="collapse navbar-collapse" id="navbarCollapse">
+				<?php
+				wp_nav_menu( array(
+					'theme_location'    => 'primary',
+					'depth'             => 2,
+					'container'         => '',
+					'container_class'   => '',
+					'container_id'      => '',
+					'menu_class'        => 'nav navbar-nav',
+					'fallback_cb'       => 'WP_Bootstrap_Navwalker::fallback',
+					'walker'            => new WP_Bootstrap_Navwalker(),
+				) );
+				?>
+			</div>
+		</nav>
