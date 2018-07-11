1ed0f730e7b2e77de32c270bd3b9cae31dd74892
Ben Kaminski <ben@benkaminski.com>
2018-07-11 12:19:54 -0400

    creatiting header and main navigation

--- front-page.php
+++ front-page.php
@@ -1,12 +1,14 @@
 <?php get_header(); ?>
 <br />
-<div class="subMenu">
-	<p class="text-center pt-4 h5 text-white">
-		<strong>
-			Call Us Today! 302-659-6593 Or Call Us Toll Free At <u>1-877-AQUAPRO</u> Like &amp; Follow Us Online!
-		</strong>
-	</p>
-</div>
+	<div class="subMenu">
+		<div class="container">
+			<p class="text-center pt-4 h3 text-white">
+				<strong>Call Us Today! <a href="tel:302.659.6953"><u>302-659-6593</u></a> Or Call Us Toll Free At <a href="tel:877.278.2776"><u>1-877-AQUAPRO</u></a> <br />Like &amp; Follow Us Online!</strong><br />
+				<a href="https://www.facebook.com/aquaproinc302" class="text-white" target="_blank"><i class="fab fa-facebook fa-fw mt-3 fa-lg"></i></a> <a href="https://twitter.com/aquaproinc302" class="text-white" target="_blank"><i class="fab fa-twitter-square fa-fw mt-3 fa-lg"></i></a>
+				<img src="<?php echo get_template_directory_uri(); ?>/lib/img/credit-cards-accepted.png" alt ="Aqua Pro accepts most major credit cards" class="img-fluid mx-auto d-block mb-3" />
+			</p>
+		</div>
+	</div>
 <br />
 <br />
 <br />
